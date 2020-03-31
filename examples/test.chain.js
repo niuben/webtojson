ea522b20d425fc00399acd36957894c735d7b834
niuben <741001695@qq.com>
2020-03-31 16:32:36 +0800

    增加测试用例

--- examples/test.chain.js
+++ examples/test.chain.js
@@ -1,22 +1,22 @@
-(async()=>{
 
-    var webtojson = require("../index");
-        
-    var shiciData = await webtojson(
-      ["https://so.gushiwen.org/gushi/xiaoxue.aspx"],
-      {
-        id: ".typecont span a",
-        title: ".typecont span a",
-        url: ".typecont span a"
-      },      
-    ).add(
-    "https://so.gushiwen.org/gushi/chuzhong.aspx",      
+  
+(async()=>{
+  var webtojson = require("../index");
+  var shiciData = await webtojson(
+    ["https://so.gushiwen.org/gushi/xiaoxue.aspx"],
     {
       id: ".typecont span a",
       title: ".typecont span a",
       url: ".typecont span a"
-    }).getData();
-      
-    console.log(shiciData);    
+    },      
+  ).add(
+  "https://so.gushiwen.org/gushi/chuzhong.aspx",      
+  {
+    id: ".typecont span a",
+    title: ".typecont span a",
+    url: ".typecont span a"
+  }).getData();
+    
+  console.log(shiciData);    
 })();
         
